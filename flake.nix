ae22c93f2de74d69f6edfa257afc81dc7fd4d64e
Andreas Zweili <andreas@zweili.ch>
2024-08-06 18:48:26 +0200

    Add Nix dev environment

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Mainly intended to provide development dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.poetry
+          pkgs.python312
+        ];
+      };
+    };
+}
